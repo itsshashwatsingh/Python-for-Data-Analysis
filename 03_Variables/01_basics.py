c0f4ee1646ed44c6aee0bccc2a40c430950fdefa
itsshashwatsingh <shashwat98k@gmail.com>
2025-05-17 22:55:46 +0530

    variables & datatypes

---
+++ 03_Variables/01_basics.py
@@ -0,0 +1,26 @@
+# In python variables are used to store data that can be used to store data that can be used to store and manupalate data throughout the program
+
+
+# dynamically typed language(no need to specify the datatype unlike statically typed language c or C++)
+name = "Shashwat" 
+age = 22
+cgpa = 7.5
+
+
+
+#rules of defining variables
+
+# start with a-z or A-Z or (_)
+# can contain letters and numbers(case sensitive)
+# age & Age are different(case sensitive)
+# Avoid using keywords like(if, for ,while -> used for loops and conditions) as variable
+
+# 34age = 4 (invalid)
+age = 32
+# age$ = 35 (invalid)
+_age = 34
+_nice_45 = 34
+a_b_c_d_7 = 34
+a_B_c_D_8 = "sam"
+
+print(a_B_c_D_8) 
---
+++ 03_Variables/02_data_types.py
@@ -0,0 +1,17 @@
+#Integers(int): 10,15 (whole numbers)
+#floats(float):5.5.3.9 (Decimal numbers)
+#strings(str): "hello","python"(text data)
+#Booleans(bool):true,false(represents true or false values)
+#lists:[1,2,3](ordered,mutable collection)
+#Tuples:(1,2,3)(ordered,immutable collection)
+#sets:{1,2,3}(unordered collection of unique elements)
+#Dictionaries:{"name":"Alice","age":22}(key value pairs)
+
+name = "Alice" #automatically identifies as string
+print(name)
+
+age = 22  #automatically identifies as int
+print(age)
+
+cgpa = 7.5  #automatically identifies as float
+print(cgpa)
