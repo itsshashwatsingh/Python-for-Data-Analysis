c14808b37453c45f3c03bb9d4ad191ae880ceb1c
itsshashwatsingh <shashwat98k@gmail.com>
2025-05-17 20:10:28 +0530

    first python code

---
+++ main.py
@@ -0,0 +1,18 @@
+print("hello world")
+
+print("how are you")
+
+print("i am fine")
+
+print(2)
+
+#indentation
+
+
+# if condition statement
+# if(a<3):
+        #statement1
+        #statement2 // these statement comes inside if condition
+
+#statement3 // this perticular statement is nit a part of if condition statement 
+
